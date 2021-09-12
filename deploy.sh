fdf07d5840ba0c93264cef0c0c9858213bb2d203
Sina Bock <sina.bock@uni-wuerzburg.de>
2021-09-12 05:20:55 +0200

    workflow testing

--- deploy.sh
+++ deploy.sh
@@ -17,9 +17,7 @@ git add -A
 git commit -m 'deploy'
 
 # if you are deploying to https://<USERNAME>.github.io
-git push -f git@github.com:OCR4all
-/
-OCR4all.github.io git master
+git push -f git@github.com:OCR4all/OCR4all.github.io git gh-pages
 
 # if you are deploying to https://<USERNAME>.github.io/<REPO>
 # git push -f git@github.com:<USERNAME>/<REPO>.git master:gh-pages
