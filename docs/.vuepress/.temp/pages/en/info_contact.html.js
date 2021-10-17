92c7b906013d7b78ab2bc60d3e51625218e45bf6
Isabel <isabel.mueller1@stud-mail.uni-wuerzburg.de>
2021-10-17 13:45:34 +0200

    info.contacts

---
+++ docs/.vuepress/.temp/pages/en/info_contact.html.js
@@ -0,0 +1,27 @@
+export const data = {
+  "key": "v-863fd586",
+  "path": "/en/info_contact.html",
+  "title": "Information and Contacts",
+  "lang": "en-US",
+  "frontmatter": {
+    "lang": "en-US",
+    "title": "Information and Contacts",
+    "description": "Links"
+  },
+  "excerpt": "",
+  "headers": [],
+  "filePathRelative": "en/info_contact.md"
+}
+
+if (import.meta.webpackHot) {
+  import.meta.webpackHot.accept()
+  if (__VUE_HMR_RUNTIME__.updatePageData) {
+    __VUE_HMR_RUNTIME__.updatePageData(data)
+  }
+}
+
+if (import.meta.hot) {
+  import.meta.hot.accept(({ data }) => {
+    __VUE_HMR_RUNTIME__.updatePageData(data)
+  })
+}
