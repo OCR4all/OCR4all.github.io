7289220b0a777e1cc5976c0ae1d90c07b2bfd491
Isabel <isabel.mueller1@stud-mail.uni-wuerzburg.de>
2021-12-04 21:21:45 +0100

    navbar

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -52,7 +52,6 @@ module.exports = {
       { text: 'User Guide',
         children: ['/en/ocr4all_userguide/0.FrontPage/0.FrontPage.md',
         '/en/ocr4all_userguide/1.OCR4all/1.1_Introduction.md',
-        '/en/ocr4all_userguide/1.OCR4all/1.2_Set up and folder structure.md',
         '/en/ocr4all_userguide/2.ScanAndImagePreparation/2-ScanPreparation.md',
         '/en/ocr4all_userguide/3.ProjectStartAndOverview/3.1-Getting started.md',
         '/en/ocr4all_userguide/3.ProjectStartAndOverview/3.2-Project Overview.md',
