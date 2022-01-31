a36871f00cd3c6400f0b7af54ae82d17229b5dd3
Isabel <isabel.mueller1@stud-mail.uni-wuerzburg.de>
2022-01-31 17:01:57 +0100

    fixed GitHub link

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -15,7 +15,7 @@ module.exports = {
     },
   },
   themeConfig: {
-    repo: 'OCR4all/OCR4all',
+    repo: 'OCR4all/OCR4all.github.io/',
     lastUpdated: false,
     contributors: false,
     locales: {
