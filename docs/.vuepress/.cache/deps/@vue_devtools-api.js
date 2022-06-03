23a17c3a6153c466a37a1d60578306516e73d0ce
Isabel <isabel.mueller1@stud-mail.uni-wuerzburg.de>
2022-06-03 11:09:52 +0200

    remove VirtualBox in Dropdown-menu

---
+++ docs/.vuepress/.cache/deps/@vue_devtools-api.js
@@ -0,0 +1,19 @@
+import {
+  isPerformanceSupported,
+  now,
+  setupDevtoolsPlugin
+} from "./chunk-ADSL4WMG.js";
+import {
+  init_define_EXTERNAL_LINK_ICON_LOCALES,
+  init_define_MZ_ZOOM_OPTIONS
+} from "./chunk-MCQLGLGH.js";
+
+// dep:@vue_devtools-api
+init_define_EXTERNAL_LINK_ICON_LOCALES();
+init_define_MZ_ZOOM_OPTIONS();
+export {
+  isPerformanceSupported,
+  now,
+  setupDevtoolsPlugin
+};
+//# sourceMappingURL=@vue_devtools-api.js.map
